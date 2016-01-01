5e784e3aa64c818457d9c424a872ac5fece8b9bf
cerf-de-pixel <julien@cerf-de-pixel.fr>
2016-01-01 02:20:05 +0100

    Suppression temporaire du bouton de partage facebook

--- script/Mathoux.js
+++ script/Mathoux.js
@@ -1,11 +1,3 @@
-// CODE GOOGLE ANALYTICS
-//(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-//ga('create', 'UA-57903562-1', 'auto');
-//ga('send', 'pageview');
-
 /**
 ** Constructeur d'un objet Mathoux
 ** @param Name : 	Nom de l'app (ex: "Calcul mental")
@@ -218,8 +210,8 @@ function UI(objMathoux) {
         menuHTML += "<hr><h2>" + objMathoux.UI.printGameScore() + "</h2></div>"
 
         // Mise en place du bouton FACEBOOK
-        menuHTML += "<p><button id='bt_partageFB' onclick='"+ objMathoux.ID +".facebookShare()'><img src='./img/facebook.png'>";
-        menuHTML += "<span> Partager mon score</span></button></p>";
+        //menuHTML += "<p><button id='bt_partageFB' onclick='"+ objMathoux.ID +".facebookShare()'><img src='./img/facebook.png'>";
+        //menuHTML += "<span> Partager mon score</span></button></p>";
 
         // Mise en place des rangs avec leurs titres et leurs niveaux (avec notes individuels)
 		var compteur_niveaux = 1;
